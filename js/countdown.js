6c0f271de52d6e458d4e82f7d1b8d2391ca52e1f
soujiang <103637149+soujiang@users.noreply.github.com>
2022-11-20 15:09:11 -0300

    hora5

--- js/countdown.js
+++ js/countdown.js
@@ -3,7 +3,7 @@ simplyCountdown('#cuenta', {
 	month: 11, // required
 	day: 20, // required
 	hours: 12, // Default is 0 [0-23] integer
-	minutes: 08, // Default is 0 [0-59] integer
+	minutes: 11, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Day',
@@ -18,7 +18,7 @@ simplyCountdown('#cuenta', {
 	// in case of inline set to false
 	enableUtc: true, //Use UTC as default
 	onEnd: function() {
-		window.location.href = "https://professor-falken.com";
+		window.location.href = "https://endearing-baklava-f7930d.netlify.app";
 		return; 
 	}, //Callback on countdown end, put your own function here
 	refresh: 1000, // default refresh every 1s
